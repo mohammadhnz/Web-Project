a16fa843a5522ef1ef842c3cb22a2d1818a7cae3
mohammadhnz0 <hmohammadali2013@gmail.com>
2023-03-06 18:27:35 +0330

    feat: add nested categories api

--- backend-core/store/urls.py
+++ backend-core/store/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('category/list', views.CategoryList.as_view(), name='product_price_change'),
+    path('nested_categories/list', views.NestedCategoriesListView.as_view(), name='nested_categories_list_view'),
     path('product/list', views.ProductList.as_view(), name='product_get_list'),
     path('product/create-or-update', csrf_exempt(views.CreateOrUpdate.as_view()), name='product_create_or_update'),
     path('product/redirect', views.Redirect.as_view(), name='product_redirect'),
