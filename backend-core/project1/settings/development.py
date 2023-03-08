e3f6f7ceac49ef820048160fc6ec036aac341268
mohammadhnz0 <hmohammadali2013@gmail.com>
2023-03-08 21:04:03 +0330

    fix: delete pycache files

--- backend-core/project1/settings/development.py
+++ backend-core/project1/settings/development.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 from decouple import config
@@ -85,6 +85,11 @@ WSGI_APPLICATION = 'project1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=4),
+    'AUTH_HEADER_TYPES': ('JWT',),
+}
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
